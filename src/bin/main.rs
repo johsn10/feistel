36eaabf130f41c15f6206062cf774d97eff87407
johsn10 <96774346+johsn10@users.noreply.github.com>
2024-08-08 20:57:46 +0200

    First working

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,17 +4,25 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
-    left: u8,
-
-    #[arg(short, long, default_value_t = 1)]
-    right: u8,
-    
+    #[arg(long)]
+    hex: String,
 }
 
 fn main() {
     let args = Args::parse();
-    let feistel_input: FeistelInput = FeistelInput::from_sides(0x12, 0x34);
-    let (left, right) = feistel_input.split();
-    println!("Left: {:04x}, Right: {:04x}, Full: {:04x}", left, right, *feistel_input);
+    let data = match u16::from_str_radix(&args.hex, 16) {
+        Ok(v) => v,
+        Err(_) => panic!("Invalid hex data"),
+    };
+
+    let feistel_input: FeistelInput = FeistelInput::new(data);
+    let feistel: Feistel = Feistel::new(feistel_input, obfuscate);
+    let run1 = feistel.run();
+    let run2 = run1.run();
+
+    println!("Original: {:04x}, Run 1: {:04x}, Run 2: {:04x}", *feistel_input, *run1.data, *run2.data);
+}
+
+fn obfuscate(data: &u8) -> u8 {
+    data ^ 0x12
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,44 @@ use derive_new::new;
 use std::ops::Deref;
 
 #[derive(Debug, Clone, Copy, new)]
+pub struct Feistel {
+    pub data: FeistelInput,
+    pub obfuscate: fn(&u8) -> u8,
+}
+
+impl Feistel {
+    pub fn run(&self) -> Feistel {
+        let feistel_output = feistel_iterator(&self);
+
+        return feistel_output;
+    }
+}
+
+fn feistel_iterator(feistel: &Feistel) -> Feistel {
+    let (mut left, mut right) = feistel.data.split();
+
+    for _ in 0..3 {
+        let next_left = right;
+
+        let right_obfuscated = (feistel.obfuscate)(&right);
+        let next_right = left ^ right_obfuscated;
+
+        (left, right) = (next_left, next_right);
+    }
+
+    let mut new_feistel = feistel.clone();
+    new_feistel.data = FeistelInput::from_sides(right, left);
+    new_feistel
+}
+
+#[derive(Debug, Clone, Copy, new)]
 pub struct FeistelInput(u16);
 
 impl FeistelInput {
     pub fn split(&self) -> (u8, u8) {
-        let left = (**self & 0x11_00 >> 8) as u8;
-        let right = (**self & 0x00_11) as u8;
+        let left = ((**self & 0xff_00) >> 8) as u8;
+        let right = (**self & 0x00_ff) as u8;
+
         (left, right)
     }
 
